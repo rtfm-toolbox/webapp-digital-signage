69c958f8f97d71f396ee1800aa7a1043b7043f3a
Jonathan Günz <jonathan.guenz@experimenta.science>
2019-09-03 00:02:00 +0200

    add dinamic update of opening times

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webapp-digital-signage';
+  content = '';
+  blubber = false;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      console.log("hour", moment().hour());
+
+      if (
+        moment().weekday() != 1 &&
+        moment().weekday() != 7 &&
+        moment().hour() > 15 &&
+        moment().hour() < 24) {
+        this.blubber = true;
+      } else {
+        this.blubber = false;
+      }
+    }, 1000);
+  }
+
 }
